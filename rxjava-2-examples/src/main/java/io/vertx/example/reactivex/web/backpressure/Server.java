168d4b46d44ee4301b322c6de440d1835deb6804
Tomasz Michalak <tomek.michalak@gmail.com>
2018-02-05 09:16:31 +0100

    Backpressure with rx client

--- rxjava-2-examples/src/main/java/io/vertx/example/reactivex/web/backpressure/Server.java
+++ rxjava-2-examples/src/main/java/io/vertx/example/reactivex/web/backpressure/Server.java
@@ -1,19 +1,23 @@
 package io.vertx.example.reactivex.web.backpressure;
 
+import io.netty.handler.codec.http.HttpStatusClass;
+import io.reactivex.Flowable;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.RequestOptions;
 import io.vertx.example.util.Runner;
 import io.vertx.reactivex.RxHelper;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.core.http.HttpClient;
+import io.vertx.reactivex.core.http.HttpClientRequest;
 import io.vertx.reactivex.core.http.HttpClientResponse;
 import io.vertx.reactivex.core.http.HttpServer;
 import io.vertx.reactivex.core.http.HttpServerRequest;
 import io.vertx.reactivex.ext.web.Router;
 import io.vertx.reactivex.ext.web.handler.BodyHandler;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author tomasz.michalak
@@ -21,7 +25,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Server extends AbstractVerticle {
 
   private static final int NUMBER_OF_REQUESTS = 1000;
-  private static final int N_THREADS = 100;
 
   // Convenience method so you can run it in your IDE
   public static void main(String[] args) throws InterruptedException {
@@ -37,6 +40,8 @@ public class Server extends AbstractVerticle {
       .end("<html><body><h1>Hello from vert.x!</h1></body></html>"));
 
     HttpServer server = vertx.createHttpServer();
+    HttpClient httpClient = vertx.createHttpClient();
+
     server.requestStream()
       .toFlowable()
       .map(HttpServerRequest::pause)
@@ -46,26 +51,41 @@ public class Server extends AbstractVerticle {
         req.resume();
         router.accept(req);
       });
-
-    server.rxListen(8080).subscribe(res -> generateRequests());
+    server.rxListen(8080).subscribe(res -> {
+      AtomicInteger success = new AtomicInteger(0);
+      AtomicInteger fail = new AtomicInteger(0);
+      Flowable.fromIterable(requests(httpClient))
+        .flatMap(r -> r)
+        .subscribeOn(RxHelper.blockingScheduler(Vertx.vertx()))
+        .subscribe(
+          resp -> {
+            if (HttpStatusClass.SUCCESS.contains(resp.statusCode())) {
+              success.incrementAndGet();
+            } else {
+              fail.incrementAndGet();
+            }
+          },
+          Throwable::printStackTrace,
+          () -> System.out.println(
+            "All requests completed [" + NUMBER_OF_REQUESTS + "], success [" + success.get()
+              + "], fail [" + fail.get() + "]"));
+    });
   }
 
-  private void generateRequests() throws InterruptedException {
-    AtomicInteger ok = new AtomicInteger(0);
-    AtomicInteger fail = new AtomicInteger(0);
-
-    List<Callable<HttpClient>> tasks = Collections.nCopies(NUMBER_OF_REQUESTS,
-      () -> vertx.createHttpClient()
-        .getNow(8080, "localhost", "/", resp ->
-          log(resp, resp.statusCode() == 200 ? ok.incrementAndGet() : ok.get(),
-            resp.statusCode() == 503 ? fail.incrementAndGet() : fail.get())
-        ));
-    Executors.newFixedThreadPool(N_THREADS).invokeAll(tasks);
-
+  private List<Flowable<HttpClientResponse>> requests(HttpClient httpClient) {
+    return Stream.generate(() -> request(httpClient)).limit(NUMBER_OF_REQUESTS)
+      .collect(Collectors.toList());
   }
 
-  private void log(HttpClientResponse response, int ok, int fail) {
-    System.out.println(response.statusCode() + ", ok=" + ok + ", fail=" + fail);
+  private Flowable<HttpClientResponse> request(HttpClient httpClient) {
+    HttpClientRequest req = httpClient.get(
+      new RequestOptions()
+        .setHost("localhost")
+        .setPort(8080)
+        .setURI("/"));
+    return req
+      .toFlowable()
+      .doOnSubscribe(subscription -> req.end());
   }
 
 }
